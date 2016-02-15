f06825e1c56d7377b2a026b52f1b8f628e9ed6a8
Marco Koivisto <Marco@Anvandares-MacBook-Pro.local>
2016-02-15 23:34:51 +0100

    Damn, so many changes

---
+++ casper/DriveScene.swift
@@ -0,0 +1,155 @@
+//
+//  GameScene.swift
+//  stick test
+//
+//  Created by Dmitriy Mitrophanskiy on 28.09.14.
+//  Copyright (c) 2014 Dmitriy Mitrophanskiy. All rights reserved.
+//
+
+import SpriteKit
+
+class DriveScene: SKScene {
+    
+    var appleNode: SKSpriteNode?
+    let setJoystickStickImageBtn = SKLabelNode(), setJoystickSubstrateImageBtn = SKLabelNode()
+    
+    var joystickStickImageEnabled = true {
+        
+        didSet {
+            
+            let image = joystickStickImageEnabled ? UIImage(named: "jStick") : nil
+            moveAnalogStick.stick.image = image
+        }
+    }
+    
+    var joystickSubstrateImageEnabled = true {
+        
+        didSet {
+            
+            let image = joystickSubstrateImageEnabled ? UIImage(named: "jSubstrate") : nil
+            moveAnalogStick.substrate.image = image
+        }
+    }
+    
+    let moveAnalogStick =  🕹(diameter: 80)
+    
+    override func didMoveToView(view: SKView) {
+
+        /* Setup your scene here */
+        backgroundColor = UIColor.whiteColor()
+        physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
+        
+        moveAnalogStick.position = CGPointMake(moveAnalogStick.radius + 15, moveAnalogStick.radius + 15)
+        addChild(moveAnalogStick)
+        moveAnalogStick.trackingHandler = { jData in
+            
+            guard let aN = self.appleNode else { return }
+            aN.position = CGPointMake(aN.position.x + (jData.velocity.x * 0.12), aN.position.y + (jData.velocity.y * 0.12))
+        }
+        
+        let btnsOffset: CGFloat = 10
+        let btnsOffsetHalf = btnsOffset / 2
+        let joystickSizeLabel = SKLabelNode(text: "Joysticks Size:")
+        joystickSizeLabel.fontSize = 20
+        joystickSizeLabel.fontColor = UIColor.blackColor()
+        joystickSizeLabel.horizontalAlignmentMode = .Left
+        joystickSizeLabel.verticalAlignmentMode = .Top
+        joystickSizeLabel.position = CGPoint(x: btnsOffset, y: self.frame.size.height - btnsOffset)
+        addChild(joystickSizeLabel)
+
+        /*
+        jSizeMinusSpriteNode.anchorPoint = CGPoint(x: 0, y: 0.5)
+        jSizeMinusSpriteNode.position = CGPoint(x: CGRectGetMaxX(joystickSizeLabel.frame) + btnsOffset, y: CGRectGetMidY(joystickSizeLabel.frame))
+        addChild(jSizeMinusSpriteNode)
+        
+        jSizePlusSpriteNode.anchorPoint = CGPoint(x: 0, y: 0.5)
+        jSizePlusSpriteNode.position = CGPoint(x: CGRectGetMaxX(jSizeMinusSpriteNode.frame) + btnsOffset, y: CGRectGetMidY(joystickSizeLabel.frame))
+        addChild(jSizePlusSpriteNode)*/
+        
+        setJoystickStickImageBtn.fontColor = UIColor.blackColor()
+        setJoystickStickImageBtn.fontSize = 20
+        setJoystickStickImageBtn.verticalAlignmentMode = .Bottom
+        setJoystickStickImageBtn.position = CGPointMake(CGRectGetMidX(self.frame), moveAnalogStick.position.y - btnsOffsetHalf)
+        addChild(setJoystickStickImageBtn)
+        
+        setJoystickSubstrateImageBtn.fontColor  = UIColor.blackColor()
+        setJoystickSubstrateImageBtn.fontSize = 20
+        setJoystickStickImageBtn.verticalAlignmentMode = .Top
+        setJoystickSubstrateImageBtn.position = CGPointMake(CGRectGetMidX(self.frame), moveAnalogStick.position.y + btnsOffsetHalf)
+        addChild(setJoystickSubstrateImageBtn)
+        
+        joystickStickImageEnabled = true
+        joystickSubstrateImageEnabled = true
+        
+        setRandomStickColor()
+        addApple(CGPointMake(CGRectGetMidX(frame), CGRectGetMidY(frame)))
+    }
+    
+    func addApple(position: CGPoint) {
+        
+        guard let appleImage = UIImage(named: "apple") else { return }
+        
+        let texture = SKTexture(image: appleImage)
+        let apple = SKSpriteNode(texture: texture)
+        if #available(iOS 8.0, *) {
+            apple.physicsBody = SKPhysicsBody(texture: texture, size: apple.size)
+            apple.physicsBody!.affectedByGravity = false
+        } else {
+            // Fallback on earlier versions
+        }
+        
+        insertChild(apple, atIndex: 0)
+        apple.position = position
+        appleNode = apple
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        /* Called when a touch begins */
+        
+        if let touch = touches.first {
+            
+            let node = nodeAtPoint(touch.locationInNode(self))
+            
+            switch node {
+                
+            /*case jSizePlusSpriteNode:
+                moveAnalogStick.diameter += 1
+                rotateAnalogStick.diameter += 1
+            case jSizeMinusSpriteNode:
+                moveAnalogStick.diameter -= 1
+                rotateAnalogStick.diameter -= 1*/
+            case setJoystickStickImageBtn:
+                joystickStickImageEnabled = !joystickStickImageEnabled
+            case setJoystickSubstrateImageBtn:
+                joystickSubstrateImageEnabled = !joystickSubstrateImageEnabled
+
+            default:
+                addApple(touch.locationInNode(self))
+            }
+        }
+    }
+    
+    func setRandomStickColor() {
+        
+        let randomColor = UIColor.random()
+        moveAnalogStick.stick.color = randomColor
+    }
+    
+    func setRandomSubstrateColor() {
+        
+        let randomColor = UIColor.random()
+        moveAnalogStick.substrate.color = randomColor
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
+
+extension UIColor {
+    
+    static func random() -> UIColor {
+        
+        return UIColor(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1)
+    }
+}
---
+++ casper/DriveViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DriveViewController.swift
+//  casper
+//
+//  Created by Marco Koivisto on 2016-02-15.
+//  Copyright © 2016 Pontus Pohl. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+class DriveViewController: UIViewController {
+    
+    let joystick = AnalogJoystick(diameter: 100)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var size = CGSizeMake(self.view.bounds.height, self.view.bounds.width)
+        let scene = DriveScene(size: size)
+        scene.backgroundColor = .whiteColor()
+        if let skView = self.view as? SKView {
+            
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            /* Set the scale mode to scale to fit the window */
+            //scene.scaleMode = .AspectFill
+            skView.presentScene(scene)
+        }
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        
+        //LightContent
+        return UIStatusBarStyle.LightContent
+        
+        //Default
+        //return UIStatusBarStyle.Default
+        
+    }
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return .Landscape
+    }
+    
+}
