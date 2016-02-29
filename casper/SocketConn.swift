50c05ee9d3994adf77c0e63a2018184f46ca2c2f
Pontus Pohl <pontus.pohl@endian.se>
2016-02-29 08:49:29 +0100

    added socket mockup class #1

--- casper/SocketConn.swift
+++ casper/SocketConn.swift
@@ -5,5 +5,62 @@
 //  Created by Pontus Pohl on 28/02/16.
 //  Copyright © 2016 Pontus Pohl. All rights reserved.
 //
-
-import Foundation
+//
+//import Foundation
+//
+//
+//
+//
+//class SocketConn:NSObject, NSStreamDelegate{
+//
+//    
+//private var input: NSInputStream?
+//private var output: NSOutputStream?
+//
+//
+//var dataToStream: NSData!
+//
+//var byteIndex: Int!
+//
+//func Connect(){
+//    
+//    let host:CFStringRef = "127.0.0.1"
+//    let port:UInt32 = 9999
+//    
+//    
+//   
+//    
+//  
+//    
+//    
+//    NSStream.getStreamsToHostWithName(host as String, port: 9999, inputStream: &input, output: &outputStream)
+//    
+//    let inputStream = input!
+//    let outputStream = output!
+//    
+//    self.inputStream = readStream!.takeRetainedValue()
+//    self.outputStream = writeStream!.takeRetainedValue()
+//    
+//    
+//    self.inputStream.scheduleInRunLoop(NSRunLoop.currentRunLoop(), forMode: NSDefaultRunLoopMode)
+//    self.outputStream.scheduleInRunLoop(NSRunLoop.currentRunLoop(), forMode: NSDefaultRunLoopMode)
+//    
+//    self.inputStream.delegate = self
+//    self.outputStream.delegate = self
+//    
+//    //inputStream.open()
+//    //outputStream.open()
+//    self.inputStream.open()
+//    self.outputStream.open()
+//    
+//    
+//    dataToStream = sendValue()
+//    var readBytes = dataToStream.bytes
+//    var dataLength = dataToStream.length
+//    
+//    var buffer = Array<UInt8>(count: dataLength, repeatedValue: 0)
+//    memcpy(UnsafeMutablePointer(buffer), readBytes, dataLength)
+//    var len = self.outputStream.write(buffer, maxLength: dataLength)
+//    
+//    }   
+//}
