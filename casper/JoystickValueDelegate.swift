9d0b6647944da55e50d7e50f86b81b497c70510e
Pontus Pohl <pontus.pohl@gmail.com>
2016-04-03 12:01:57 +0200

    ADD Prototype Delegate for future use

---
+++ casper/JoystickValueDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  JoystickValueDelegate.swift
+//  casper
+//
+//  Created by Pontus Pohl on 03/04/16.
+//  Copyright © 2016 Pontus Pohl. All rights reserved.
+//
+
+import Foundation
+protocol JoystickValueDelegate: class{
+    
+    
+    
+    func DidMoveJoystick(sender : JoystickValueDelegate)
+}
