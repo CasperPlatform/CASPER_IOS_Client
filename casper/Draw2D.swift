b3254575d808ac20a692ae7909001defd012f668
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-01 11:35:32 +0200

    Add : Mockupstuff for mapping

---
+++ casper/Draw2D.swift
@@ -0,0 +1,21 @@
+//
+//  Draw2D.swift
+//  casper
+//
+//  Created by Pontus Pohl on 24/04/16.
+//  Copyright © 2016 Pontus Pohl. All rights reserved.
+//
+
+import UIKit
+
+class Draw2D: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ casper/LidarMapperDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  LidarMapperDelegate.swift
+//  casper
+//
+//  Created by Pontus Pohl on 24/04/16.
+//  Copyright © 2016 Pontus Pohl. All rights reserved.
+//
+
+import Foundation
